887b22f01a152b0963a3f8f3a73021c4418d8b05
ryood <kawanabe@gmail.com>
2015-05-26 13:29:37 +0900

    first commit

---
+++ DDS_Kick_Test01/DDS_Kick_Test01.cpp
@@ -0,0 +1,103 @@
+// DDS_Kick_Test01.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <stdint.h>
+#include <math.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <io.h>
+#include <fcntl.h>
+
+#include "WaveTable.h"
+
+/*********************************************************
+lookupTable     : 16bit (use 12bit)
+
+wavePhaseRegister   : 32bit
+waveTunigWord       : 32bit
+
+//ampAmount       : 8bit
+//toneAmount      : 8bit
+decayAmount     : 8bit
+//bpmAmount       : 8bit
+**********************************************************/
+
+#define SAMPLE_CLOCK		44100	// 44.1kHz
+#define LFO_SAMPLE_CLOCK	441		// 441Hz
+
+#define LOOKUP_TABLE_SIZE	1024	// Lookup Table 1個のバイト長: 10bit
+#define LOKKUP_TABLE_N		2		// Lookup Table の個数
+
+uint16_t *lookupTable[LOOKUP_TABLE_SIZE];
+
+// Basic Wave
+uint32_t wavePhaseRegister;
+uint32_t waveTuningWord;
+int32_t  waveValue;
+
+// Decay
+uint32_t decayPhaseRegister;
+uint32_t decayTuningWord;
+uint8_t  decayAmount = 255;
+
+// frequency > SAMPLE_CLOCK / 2^32 = about 10.27uHz
+float waveFrequency = 1000.0f;
+float lfoFrequency = 10.0f;
+
+int period = 442;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	uint32_t lfoCount = 0;
+	int32_t amValue;
+
+
+	_setmode(_fileno(stdout), _O_BINARY);
+
+	// lookupTable の準備
+	lookupTable[0] = waveTableSine;
+	lookupTable[1] = waveTableSawtoothUp;
+
+	// DDS用の変数の初期化
+	waveTuningWord = waveFrequency * pow(2.0f, 32) / SAMPLE_CLOCK;
+	decayTuningWord = lfoFrequency * pow(2.0f, 32) / SAMPLE_CLOCK;
+	wavePhaseRegister = 0;
+	decayPhaseRegister = 0;
+
+	for (int i = 0; i < period; i++)
+	{
+		// LFO系の処理
+		if (lfoCount++ > SAMPLE_CLOCK / LFO_SAMPLE_CLOCK)
+		{
+			lfoCount = 0;
+			decayPhaseRegister += decayTuningWord;
+			printf("decayPhaseRegister: %d\n", decayPhaseRegister);
+
+			// lookupTableのバイト長に丸める
+			// 32bit -> 10bit
+			uint16_t decayIndex = decayPhaseRegister >> 22;
+			//printf("decayIndex: %d\n", decayIndex);
+
+			amValue = *(lookupTable[1] + decayIndex);
+			//printf("amValue: %d\n", amValue);
+		}
+		wavePhaseRegister += waveTuningWord;
+		//printf("wavePhaseRegister: %d\n", wavePhaseRegister);
+
+		// lookupTableのバイト長に丸める
+		// 32bit -> 10bit
+		uint16_t index = (wavePhaseRegister) >> 22;
+		//printf("index: %d\n", index);
+
+		waveValue = (*(lookupTable[0] + index) << 4) - 0x8000;
+		printf("%d\n", waveValue);
+
+		// 16bit長の raw データとして出力
+		//fwrite(&waveValue, sizeof(waveValue), 1, stdout);
+	}
+
+	return 0;
+}
